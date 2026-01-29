c26eefb25d4919fae554c1a11aaee0d508e4eb09
Jesus Ricardo Ortega Sanchez <jesus.ortega216703@potros.itson.edu.mx>
2026-01-28 22:51:57 -0700

    Enable CORS for Angular frontend

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,15 @@ use App\Http\Controllers\MutationController;
 use App\Http\Controllers\StatsController;
 use App\Http\Controllers\ListController;
 
-Route::post('/mutation', [MutationController::class, 'detect']);
-Route::get('/stats', [StatsController::class, 'index']);
-Route::get('/list', [ListController::class, 'index']);
+Route::middleware(function ($request, $next) {
+    return $next($request)
+        ->header('Access-Control-Allow-Origin', 'http://localhost:4200')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+})->group(function () {
+
+    Route::post('/mutation', [MutationController::class, 'detect']);
+    Route::get('/stats', [StatsController::class, 'index']);
+    Route::get('/list', [ListController::class, 'index']);
+
+});
