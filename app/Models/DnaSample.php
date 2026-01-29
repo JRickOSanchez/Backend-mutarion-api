ea0b1ad60efa38a56d35e76c279a2d2a4f3e69f8
Jesus Ricardo Ortega Sanchez <jesus.ortega216703@potros.itson.edu.mx>
2026-01-28 22:01:27 -0700

    Fix migration: add dna_sequence column

--- app/Models/DnaSample.php
+++ app/Models/DnaSample.php
@@ -10,8 +10,13 @@ class DnaSample extends Model
 {
     use HasFactory;
 
+    protected $table = 'dna_samples';
     protected $fillable = [
         'dna_hash',
         'has_mutation',
+    ];
+    protected $casts = [
+        'dna_sequence' => 'array',
+        'has_mutation' => 'boolean'
     ];
 }
